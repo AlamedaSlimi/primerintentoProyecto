d1d3d6f8ef1e638587534be4c648f26fb4816df8
AlamedaSlimi <89815816+AlamedaSlimi@users.noreply.github.com>
2023-11-26 00:45:15 -0800

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: "https://primerintentoprofinapi.onrender.com/api"
+    baseURL: "https://primerintentoprofinapi.onrender.com/api/"
 })
 
