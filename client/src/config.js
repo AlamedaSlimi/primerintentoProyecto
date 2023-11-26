8c7a8493b3f13603a3ccf014f2944b4ecc303180
AlamedaSlimi <89815816+AlamedaSlimi@users.noreply.github.com>
2023-11-26 00:23:43 -0800

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: "https://daily-diary-blog.herokuapp.com/api/"
+    baseURL: "https://primerintentoprofinapi.onrender.com/"
 })
 
